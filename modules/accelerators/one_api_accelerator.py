5bdbe318b6805d2bb52d4621ea1e6f9ce6607e12
Andrew Powers-Holmes <aholmes@omnom.net>
2023-02-14 14:47:25 +1100

    add fastapi pinned version to requirements and reupdate oneapi to add missing tweaks

--- modules/accelerators/one_api_accelerator.py
+++ modules/accelerators/one_api_accelerator.py
@@ -8,7 +8,7 @@ class OneApiAccelerator(BaseAccelerator):
     _instance = None
 
     def __init__(self):
-        self.device = torch.device(MPSAccelerator.device_string)
+        self.device = torch.device(OneApiAccelerator.device_string)
         return
 
     @classmethod
@@ -62,6 +62,7 @@ class OneApiAccelerator(BaseAccelerator):
 
     def enable_tf32(self):
         try:
+            print("setting tf32 mode")
             ipex.set_fp32_math_mode(device="xpu", mode=ipex.FP32MathMode.TF32)
             #ipex.set_fp32_math_mode(device="xpu", mode=ipex.FP32MathMode.BF32)
         except Exception:
